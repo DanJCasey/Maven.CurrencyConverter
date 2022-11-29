b3379e88887de98cc7f1f0912b8b3c5ffd080188
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-11-29 14:17:01 -0500

    all code written, all tests passing, all currency converted

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -1,7 +1,13 @@
 package io.zipcoder.currencyconverterapplication;
 
-public class CurrencyConverter {
+public class CurrencyConverter implements ConvertableCurrency {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
         return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
+    }
+
+
+    @Override
+    public CurrencyType getType() {
+        return null;
     }
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/ChineseYR.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/ChineseYR.java
@@ -1,6 +1,18 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class ChineseYR implements ConvertableCurrency {
+    private CurrencyType type = CurrencyType.CHINESE_YR;
+    @Override
+    public Double convert(CurrencyType currency) {
+        Double universalAmount = currency.getRate() / CurrencyType.CHINESE_YR.getRate();
+        return universalAmount;
+    }
+
+    @Override
+    public CurrencyType getType() {
+        return type;
+    }
 }
